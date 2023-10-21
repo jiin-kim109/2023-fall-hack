c60afcd1c72c4abb6759862b2173be8ad4109c2b
jpa118 <jpa118@sfu.ca>
2023-10-21 16:08:06 -0700

    main

--- src/containers/MainPage.jsx
+++ src/containers/MainPage.jsx
@@ -1,55 +1,83 @@
 import React from 'react';
-import Logo from '../components/Logo';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
+import '@fontsource/roboto/300.css';
 
-
-const mWrapper = styled.div`
+const MWrapper = styled.div`
     display: flex;
     flex: 1;
-    padding: 50px;
-    flex-direction: column;
-    borderStyle: dashed;
-    margin: 
-    justify-content: center;
-    align-items: center;
-`;
-
-const dWrapper = styled.div`
-    display: flex;
-    flex: 1;
-    margin: 30px;
-    borderStyle: dashed;
+    padding: 10px;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
+const DWrapper = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid blue;
+`;
+
+const DinWrapper = styled.div`
+    display: flex;
+    flex = 1;
+    width: 100%;
+    border: 2px solid red;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+`;
 
 const Maintitle = styled.div`
     text-align: center;
     flex: 1;
-    margin: 30px;
+    margin: 20px;
 `;
+
+const titlefont = {
+    fontSize: 50
+
+}
+
+const desfont = {
+    fontSize: 30
+}
 
 const Description = styled.div`
     text-align: center;
+    padding: 10px;
 `;
-
 
 function MainPage() {
     const navigate = useNavigate();
     return (
         <div>
-            <mWrapper>
-                <Maintitle>Rediscover Your Life with AI Guidance</Maintitle>
-                <Maintitle>Trusted by 5 travellers</Maintitle>
-            </mWrapper>
-            <dWrapper>
-                <Description>Enjoy your moment in foreign countries </Description>
-                <Button variant="contained" onClick={() => navigate("/q/1")}>Get Started</Button>
-            </dWrapper>
+            <MWrapper>
+                <Maintitle>
+                    <div>
+                        <p style={titlefont}>Rediscover Your Life {"\n"}
+                        with AI Guidance  </p>
+                        Trusted by 5 travellers
+                    </div>
+                </Maintitle>
+            </MWrapper>
+            <DWrapper>
+                <DinWrapper>
+                    <Description>
+                    <p style={desfont}>Enjoy your moment in foreign countries </p>
+                    TurnOverNavi AI-based generator recommends 
+    the best place to live for all durations, displaying 
+    pinpoint locations on a map
+                    </Description>
+                </DinWrapper>
+                <DinWrapper>
+                    <Button variant="contained" onClick={() => navigate("/q/1")}>Start your trip</Button>
+                </DinWrapper>
+            </DWrapper>
         </div>
     );
 }
