df8a9ba2a10b95580eb8bc3989bc04c30c77b13a
TommyOh0428 <koa18@sfu.ca>
2023-10-21 16:15:37 -0700

    add google map

--- src/containers/ResultPage.jsx
+++ src/containers/ResultPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import DataGrid from 'react-data-grid';
 import styled from 'styled-components';
+import env from 'react-dotenv';
 
 const RowWrapper = styled.div`
     display: flex;
@@ -18,50 +20,24 @@ const ColumnWrapper = styled.div`
 `;
 
 const columns = [
-    { field: 'id', headerName: '#', width: 90 },
-    {
-      field: 'firstName',
-      headerName: 'country',
-      width: 130,
-      editable: true,
-    },
-    {
-      field: 'lastName',
-      headerName: 'Region',
-      width: 130,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Budget',
-      type: 'number',
-      width: 130,
-      editable: true,
-    }
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null }
-  ];
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' }
+];
+
+const rows = [
+  { id: 0, title: 'Example' },
+  { id: 1, title: 'Demo' }
+];
 
 function DataTable () {
-    // <DataGrid rows={rows} columns={columns} pageSize={5} />
-    return (
-        <div style={{ height: 400, width: '100%' }}>
-            {null}
-        </div>
-    )
+    return <DataGrid columns={columns} rows={rows} />
 }
 
 function GoogleMap() {
     return (
         <div style={{ height: '100vh', width: '100%' }}>
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY' }}
+                    bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
                     defaultCenter={{ lat: 59.95, lng: 30.33 }}
                     defaultZoom={11}
                 >
