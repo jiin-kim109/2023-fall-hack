5f5e5777caca27fbcabb841ac71b03e9116626d5
Jiin Kim <jiinkim@d207-023-195-115.wireless.sfu.ca>
2023-10-21 12:23:17 -0700

    parser

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -2,6 +2,13 @@
  * 
  * @param {*} template string (outputtext)
  * @param {*} values array [ { key: string, replaceTo: string | number }, ]
+ * @example
+ *  expect(parse(
+ *      "We have ${a} friends in total and our budget limit is ${b}",
+ *      [ { key: "a", replaceTo: 5 }, { key: "b", replaceTo: 10 } ]
+ *  ),
+ *  "We have 5 friends in total and our budget limit is 10"
+ *  );
  */
 function parse(template, values) {
 
